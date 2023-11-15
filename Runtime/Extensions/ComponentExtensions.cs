95f6e25a7ca8a3b5961870e5933202c7515650ce
bittinkerer <bittinkerer@gmail.com>
2023-11-14 21:46:29 -0800

    Check for null before hiding/unhiding component

--- Runtime/Extensions/ComponentExtensions.cs
+++ Runtime/Extensions/ComponentExtensions.cs
@@ -4,10 +4,23 @@ namespace Packages.Estenis.UnityExts_
 {
     public static class ComponentExtensions
     {
-        public static void UnhideInInspector(this Component component) =>
+        public static void UnhideInInspector(this Component component)
+        {
+            if(component == null)
+            {
+                return;
+            }
             component.hideFlags = HideFlags.None;
+        }
 
-        public static void HideInInspector(this Component component) =>
+        public static void HideInInspector(this Component component)
+        {
+            if(component == null)
+            {
+                return;
+            }
             component.hideFlags |= HideFlags.HideInInspector;
+        }
+            
     }
 }
