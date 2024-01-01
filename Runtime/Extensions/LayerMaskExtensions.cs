56255ca40fcdd156ff7f36699af62ede3c3b1c02
bittinkerer <bittinkerer@gmail.com>
2024-01-01 10:48:07 -0800

    Add extension for getting whether layer is in layerMask

---
+++ Runtime/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Packages.Estenis.UnityExts_
+{
+    public static class LayerMaskExtensions
+    {
+        public static bool ContainsLayer(this int layerMask, int layer) =>
+            ((1 << layer) & layerMask) != 0;
+    }
+}
