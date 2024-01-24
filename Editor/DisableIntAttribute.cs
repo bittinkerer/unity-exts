621e4666a43be0d077ec93309c2545962aaed95b
bittinkerer <bittinkerer@gmail.com>
2024-01-23 22:05:55 -0800

    Add component-drawer for drawing disabled fields on components for int's

---
+++ Editor/DisableIntAttribute.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Editors
+{
+    public class DisableIntAttribute : PropertyAttribute
+    {
+    }
+}
---
+++ Editor/DisabledIntPropertyDrawer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.Editors
+{
+    [CustomPropertyDrawer(typeof(DisableIntAttribute))]
+    public class DisabledIntPropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            IntegerField integerField = new IntegerField(property.displayName)
+            { 
+                value = property.intValue,
+            };
+            integerField.SetEnabled(false);
+            return integerField;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.IntField(position, label, property.intValue);
+            GUI.enabled = true;
+        }
+    }
+}
