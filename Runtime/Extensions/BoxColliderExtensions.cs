871496e5cbd571a84d825b0a716fe03b979fcb75
bittinkerer <bittinkerer@gmail.com>
2024-02-10 15:33:32 -0800

    Box coll extensions for creating rect collider

--- Runtime/Extensions/BoxColliderExtensions.cs
+++ Runtime/Extensions/BoxColliderExtensions.cs
@@ -10,6 +10,10 @@ namespace Packages.Estenis.UnityExts_
 
         public static Vector3 WorldRightPosition(this BoxCollider boxCollider) => WorldCenterPosition(boxCollider) + new Vector3(0, 0, BoxSize(boxCollider).z / 2);
 
+        public static Vector3 WorldBottomPosition(this BoxCollider boxCollider) => WorldCenterPosition(boxCollider) - new Vector3(0, BoxSize(boxCollider).y / 2, 0);
+
+        public static Vector3 WorldTopPosition(this BoxCollider boxCollider) => WorldCenterPosition(boxCollider) + new Vector3(0, BoxSize(boxCollider).y / 2, 0);
+
         public static Vector3 BoxSize(this BoxCollider boxCollider) => boxCollider.bounds.size;
     }
 }
